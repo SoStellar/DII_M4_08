6c3e34072edc292231f45c5d70d5096d3d5d2a4d
SoStellar <githubpkaddress@gmail.com>
2022-05-19 15:19:28 +0700

    Bootstrap Notice add

---
+++ script.js
@@ -0,0 +1,16 @@
+var searchButton = document.getElementById('searchButton')
+var inputText = document.getElementById('inputText')
+var output = document.getElementById('output')
+
+function addText(){
+    let text = inputText.value
+    console.log(text)
+    let newButton = document.createElement('button')
+    newButton.classList.add('btn')
+    newButton.classList.add('btn-outline-primary')
+    newButton.classList.add('m-2')
+    newButton.setAttribute('type','button')
+    newButton.innerText =text
+    output.appendChild(newButton)
+}
+searchButton.addEventListener('click',addText)
